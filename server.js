00e3828d7b9a3e28e4cbfae0be16c7c0e5788996
highscoretech <highscoretechng@gmail.com>
2023-11-18 14:24:52 +0100

    mine manual game

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get("/", (req, res)=>{
 })
 
 mongoose.set('strictQuery', false);
-const dbUri = `mongodb://localhost:27017/dpp`
-// const dbUri = `mongodb+srv://valiantjoe:jspW8bJDiu5lnvc4@highscore.muku4gg.mongodb.net/dotplayplay?retryWrites=true&w=majority`
+// const dbUri = `mongodb://localhost:27017/dpp`
+const dbUri = `mongodb+srv://valiantjoe:jspW8bJDiu5lnvc4@highscore.muku4gg.mongodb.net/dotplayplay?retryWrites=true&w=majority`
 mongoose.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true })
     .then((result)=>  console.log('Database connected'))
     .catch((err)=> console.log(err))
