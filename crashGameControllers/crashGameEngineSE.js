2a4f43ebc3908f50a11e314a6630f0c0d6e2a99f
highscoretech <highscoretechng@gmail.com>
2023-12-11 07:34:44 +0100

    mines

--- crashGameControllers/crashGameEngineSE.js
+++ crashGameControllers/crashGameEngineSE.js
@@ -717,7 +717,7 @@ class CrashGameEngine {
       let speed = 0.01
       let trigger = 1
       let triggerEk = 1
-      game.HandlecrashCurve(34)
+      // game.HandlecrashCurve(34)
       this.multiplier = setInterval(async () => {
         if (multiplierEL >= crash_point.crashpoint) {
           clearInterval(this.multiplier);
@@ -732,7 +732,7 @@ class CrashGameEngine {
           clearInterval(this.crashCurve)
           setTimeout(() => {
             game.startCountDown(5);
-            game.fetchPreviousCrashHistory(crash_point)
+            // game.fetchPreviousCrashHistory(crash_point)
             this.load_animate = 100
           }, 3000);
         }
