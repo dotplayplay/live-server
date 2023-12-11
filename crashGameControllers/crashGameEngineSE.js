de25714e135f32754db00d7b02bbadde375c400f
highscoretech <highscoretechng@gmail.com>
2023-12-11 04:18:53 +0100

    mines

--- crashGameControllers/crashGameEngineSE.js
+++ crashGameControllers/crashGameEngineSE.js
@@ -341,7 +341,7 @@ class CrashGameEngine {
       let count = 0
       this.crashCurve = setInterval(() => {
         if (count < 590) {
-          count += 0.64
+          count += 0.54
         } else {
           count = 586.6
         }
